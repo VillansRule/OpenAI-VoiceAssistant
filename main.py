0b56883b815b51a94f70614b979eb1245a83812a
hecker <104080125+VillansRule@users.noreply.github.com>
2023-12-26 21:50:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import pygame
 import time
 import os
+import speech_recognition as sr
 
 # Set your OpenAI API key
 openai.api_key = "your_openai_api_key"
@@ -41,6 +42,19 @@ def save_history(history):
     with open(history_file_path, "w") as file:
         file.writelines(history)
 
+# Function to recognize speech using the microphone
+def recognize_speech():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something:")
+        try:
+            audio = recognizer.listen(source, timeout=5)
+            print("Recognizing...")
+            return recognizer.recognize_google(audio)
+        except sr.UnknownValueError:
+            print("Sorry, I couldn't understand.")
+            return ""
+
 # Main conversation loop
 def conversation():
     print("Welcome! Type 'exit' to end the conversation.")
@@ -49,6 +63,12 @@ def conversation():
 
     while user_input.lower() != "exit":
         user_input = input("You: ")
+
+        if user_input.lower() == "voice":
+            # Recognize speech from the microphone
+            user_input = recognize_speech()
+        elif user_input.lower() == "exit":
+            break
 
         # Generate OpenAI response
         prompt = f"You said: {user_input}\n"
